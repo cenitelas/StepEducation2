2aded580b0a1481d163b62c9e49231bb6138e99a
suyun <sarbayev.s@chocofood.kz>
2020-06-11 21:30:33 +0600

    Added MainActivity, and button on Click processing

--- app/src/main/java/kz/education/stepeducation/MainActivity.kt
+++ app/src/main/java/kz/education/stepeducation/MainActivity.kt
@@ -1,12 +1,34 @@
 package kz.education.stepeducation
 
 import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    var buttonAction: Button? = null
+    var textviewStatus: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initializeViews()
+        initializeListeners()
+    }
+
+    fun initializeViews(){
+        buttonAction = findViewById(R.id.button_activity_main_action)
+        textviewStatus = findViewById(R.id.textview_activity_main_status)
+    }
+
+    fun initializeListeners(){
+        buttonAction!!.setOnClickListener(View.OnClickListener {
+            textviewStatus!!.setText(getString(R.string.acitvity_main_textview_status_pressed))
+            textviewStatus!!.setTextColor(ContextCompat.getColor(this, R.color.colorAccent))
+        })
     }
 }
